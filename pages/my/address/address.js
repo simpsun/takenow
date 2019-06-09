8890d2b5052ba48d8087f9589fbddd5b84c8b817
simpsun <oncwnuO4qbkQpI3ST2gMKlcdMQuw@git.weixin.qq.com>
2019-06-09 13:26:06 +0800

    0.2

--- pages/my/address/address.js
+++ pages/my/address/address.js
@@ -1,164 +1,101 @@
-// pages/my/address/address.js
-
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-        index:null,
-        source: 0,
-        addressList: []
+    index: null,
+    source: 0,
+    addressList: []
   },
-  
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-    var arr = wx.getStorageSync('addressList') || [];
-
-    console.info("缓存数据：" + arr);
-
-    // 更新数据  
-
-    this.setData({
-
+  onLoad: function(t) {
+    var arr = wx.getStorageSync("addressList") || [];
+    console.info("缓存数据：" + arr), this.setData({
       addressList: arr
-
     });
-
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  onReady: function() {},
+  onShow: function() {
     this.onLoad();
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-  // 显示对话框
-  showModal(e) {
-  
+  onHide: function() {},
+  onUnload: function() {},
+  onPullDownRefresh: function() {},
+  onReachBottom: function() {},
+  onShareAppMessage: function() {},
+  showModal: function(t) {
     this.setData({
-      modalName: e.currentTarget.dataset.target,
-      addressIndex:e.currentTarget.dataset.id
-    })
-
+      modalName: t.currentTarget.dataset.target,
+      addressIndex: t.currentTarget.dataset.id
+    });
   },
-  // 关闭对话框
-  hideModal(e) {
+  hideModal: function(t) {
     this.setData({
       modalName: null
-    })
+    });
   },
-  // 删除地址
-  deleteAddress(e){
-    console.log(this.data.addressList[e.target.dataset.dd].default);
-    if (this.data.addressList[e.target.dataset.dd].default)
-    {
-      wx.showModal({
-        content:'不能删除默认地址',
-         showCancel: false,
-        confirmColor:'#F6275C'
+  deleteAddress: function(t) {
+    console.log(this.data.addressList)
+    if (this.data.addressList[t.target.dataset.dd].default == true) {
+      wx.showToast({
+        title: "不能删除默认地址",
+        icon: "none",
+        duration: 1e3
       })
-
-    }
-    else
-    {
-    this.data.addressList.splice(e.target.dataset.dd,1);
-    }
-    if (this.data.addressList.length > 0) 
-    {
-      this.setData({ addressList: this.data.addressList});
-      wx.setStorageSync('addressList', this.data.addressList);
-
     } else {
-
-      this.setData({addressList: this.data.addressList });
-      wx.setStorageSync('addressList', []);
-
+      this.data.addressList.splice(t.target.dataset.dd, 1),
+        wx.showToast({
+          title: "删除成功",
+          icon: "success",
+          duration: 1e3,
+          success: function() {
+            setTimeout(function() {}, 1e3);
+          }
+        })
     }
-    this.setData({ modalName: null })
-  
-    },
-  newAddress(){
+    if (this.data.addressList.length > 0) {
+      this.setData({
+        addressList: this.data.addressList
+      });
+      try {
+        wx.setStorageSync("addressList", this.data.addressList);
+      } catch (t) {}
+    } else {
+      this.setData({
+        addressList: this.data.addressList
+      });
+      try {
+        wx.setStorageSync("addressList", []);
+      } catch (t) {}
+    }
+    this.setData({
+      modalName: null
+    });
+  },
+  newAddress: function() {
     wx.navigateTo({
-      url: 'addressmananger',
-      success: function(res) {console.log(res);},
-      fail: function(res) {console.log(res);},
-    })
+      url: "addressmananger",
+      success: function(t) {
+        console.log(t);
+      },
+      fail: function(t) {
+        console.log(t);
+      }
+    });
   },
-  // ListTouch触摸开始
-  ListTouchStart(e) {
+  ListTouchStart: function(t) {
     this.setData({
-      ListTouchStart: e.touches[0].pageX
-    })
+      ListTouchStart: t.touches[0].pageX
+    });
   },
-
-  // ListTouch计算方向
-  ListTouchMove(e) {
+  ListTouchMove: function(t) {
     this.setData({
-      ListTouchDirection: e.touches[0].pageX - this.data.ListTouchStart > 0 ? 'right' : 'left'
-    })
+      ListTouchDirection: t.touches[0].pageX - this.data.ListTouchStart > 0 ? "right" : "left"
+    });
   },
-
-  // ListTouch计算滚动
-  ListTouchEnd(e) {
-    if (this.data.ListTouchDirection == 'left') {
-      this.setData({
-        modalName: e.currentTarget.dataset.target
-      })
-    } else {
-      this.setData({
-        modalName: null
-        
-      })
-    }
-    this.setData({
+  ListTouchEnd: function(t) {
+    "left" == this.data.ListTouchDirection ? this.setData({
+      modalName: t.currentTarget.dataset.target
+    }) : this.setData({
+      modalName: null
+    }), this.setData({
       ListTouchDirection: null
-    })
+    });
   }
-})
+});
--- pages/my/address/addressmananger.js
+++ pages/my/address/addressmananger.js
@@ -1,97 +1,189 @@
-// pages/my/address/addressmananger.js
 var addressList = null;
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    
-    region: ['天津市', '天津市', '西青区'],
+    defaultAddress: "",
+    latitude: 39.14111,
+    longitude: 117.00739,
+    isLoaction: !1,
+    edit: -1
   },
-
-  RegionChange: function (e) {
+  formSubmit: function(e) {
+    if ("" == e.detail.value.name) wx.showToast({
+      title: "请填写收货人",
+      icon: "none",
+      duration: 1e3
+    });
+    else if ("" == e.detail.value.mobile) wx.showToast({
+      title: "请填写收货电话",
+      icon: "none",
+      duration: 1e3
+    });
+    else if (/^1(3|4|5|7|8|9)\d{9}$/.test(e.detail.value.mobile))
+      if ("" == e.detail.value.defaultAddress) wx.showToast({
+        title: "请选择收货地址",
+        icon: "none",
+        duration: 1e3
+      });
+      else if ("" == e.detail.value.exactAddress) wx.showToast({
+      title: "请填写详细地址",
+      icon: "none",
+      duration: 1e3
+    });
+    else {
+      var arr = wx.getStorageSync("addressList") || [],
+         name = e.detail.value.name,
+         mobile = e.detail.value.mobile,
+          defaultAddress = e.detail.value.defaultAddress,
+          exactAddress = e.detail.value.exactAddress,
+          def = e.detail.value.def
+      console.log("arr,{}", arr),
+        addressList = {
+          name: name,
+          mobile: mobile,
+          defaultAddress: defaultAddress,
+          exactAddress: exactAddress,
+          default: def
+        }
+      if (def == true) {
+        if (arr[0].default != undefined) {
+          arr[0].default = false
+        }
+        if (this.data.edit != -1) {
+          arr.splice(this.data.edit, 1)
+        }
+        arr.unshift(addressList)
+      } else {
+        this.data.edit == -1 ? arr.push(addressList) : arr[this.data.edit] = addressList
+      }
+      try {
+        wx.setStorageSync("addressList", arr);
+      } catch (e) {}
+      wx.showToast({
+        title: "保存成功",
+        icon: "success",
+        duration: 1e3,
+        success: function() {
+          console.log(addressList)
+          setTimeout(function() {
+            wx.navigateBack({
+              delta: 1
+            });
+          }, 1e3);
+        }
+      });
+    } else wx.showToast({
+      title: "手机号码格式不对",
+      icon: "none",
+      duration: 1e3
+    });
+  },
+  judgeGrant: function() {
+    var that = this;
+    if (this.data.isLocation == true) {
+      wx.chooseLocation({
+        latitude: that.data.latitude,
+        longitude: that.data.longitude,
+        scale: 18,
+        success: function(e) {
+          console.log(e),
+            that.setData({
+              defaultAddress: e.name
+            })
+        }
+      })
+    } else {
+      this.setData({
+        modalName: "DialogModal"
+      });
+    }
+  },
+  showModal: function(t) {
     this.setData({
-      region: e.detail.value
-    })
+      modalName: t.currentTarget.dataset.target,
+      addressIndex: t.currentTarget.dataset.id
+    });
   },
-formSubmit(e){
-var name =  e.detail.value.name;
-var mobile =e.detail.value.mobile;
-var area = e.detail.value.area;
-var exactAddress = e.detail.value.exactAddress;
-var def = e.detail.value.def;
-var arr = wx.getStorageSync('addressList') || [];
-
-  console.log("arr,{}", arr);
-
-  addressList = {
-    name: name,
-    mobile: mobile,
-    area: exactAddress,
-    addressName:area,
-    address: area,
-    default:def
-  }
-  arr.push(addressList);
-  wx.setStorageSync('addressList', arr);
-  wx.navigateBack({})
-
-
-},
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  hideModal: function(t) {
+    this.setData({
+      modalName: null
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getMyLocation: function(t) {
+    this.setData({
+      modalName: null
+    });
+    var that = this;
+    if (t.detail.authSetting["scope.userLocation"]) {
+      that.setData({
+        isLocation: !0
+      }), wx.getLocation({
+        type: "gcj02",
+        success: function(t) {
+           a = t.latitude,
+           o = t.longitude;
+          that.setData({
+            latitude: a,
+            longitude: o
+          }), wx.chooseLocation({
+            latitude: a,
+            longitude: o,
+            scale: 18,
+            success: function(t) {
+              console.log(t),
+                that.setData({
+                defaultAddress: t.name
+                });
+            }
+          });
+        }
+      })
+    } else {
+      that.setData({
+        isLocation: !1
+      });
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  onLoad: function(e) {
+    console.log(e.index)
+    var that = this;
+    wx.getLocation({
+        type: "gcj02",
+        success: function(t) {
+          console.log(t);
+          var latitude = t.latitude,
+            longitude = t.longitude;
+          that.setData({
+            isLocation: 1,
+            latitude: latitude,
+            longitude: longitude
+          });
+        },
+        fail: function(t) {
+          console.log("拒绝授权"), that.setData({
+            isLocation: 0
+          });
+        }
+      })
+      if(e.index!=undefined) {
+      var arr= wx.getStorageSync("addressList");
+      console.log(arr[e.index]),
+   
+      this.setData({
+        edit: e.index,
+        name: arr[e.index].name,
+        mobile: arr[e.index].mobile,
+        defaultAddress: arr[e.index].defaultAddress,
+        exactAddress: arr[e.index].exactAddress,
+        default: arr[e.index].default
+      });
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+  onReady: function() {},
+  onShow: function() {},
+  onHide: function() {},
+  onUnload: function() {},
+  onPullDownRefresh: function() {},
+  onReachBottom: function() {},
+  onShareAppMessage: function() {}
+});
