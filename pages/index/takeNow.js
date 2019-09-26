242cc2e9575fe9d8d3857a9c733b3976759be34c
simpsun <oncwnuO4qbkQpI3ST2gMKlcdMQuw@git.weixin.qq.com>
2019-09-26 18:31:47 +0800

    跑腿费模块更新

--- pages/index/takeNow.js
+++ pages/index/takeNow.js
@@ -11,7 +11,6 @@ Page({
     longitude: 117.00739,
     isLoaction: 0,
     remarkInput: "",
-    goodsAddressTypeSelected: 0,
     AddressList: [],
     receivingAddressList: [],
     receivingAddress: "",
@@ -19,36 +18,107 @@ Page({
     switchTakeList: ['指定地点', '就近购买'],
     addrDeliverText: '送到哪里去',
     addrPurchaseText: '在哪里购买',
+    selectedCostTag:2,
+    userDeterminedCost:undefined,
+    userFilledOtherCost:0,
+    OtherCostInputStaus:0,
+    focusedOtherCost:false,
+    checkbox: [{
+      value: 0,
+      money: 1,
+      hot: true,
+    }, {
+      value: 1,
+      money: 3, 
+      hot: false,
+    }, {
+      value: 2,
+        money:5,
+      hot: true,
+    }, {
+      value: 3,
+        money: 10,
+      hot: false,
+    }, {
+      value: 4,
+        money: 20,
+      hot: false,
+    }, {
+      value: 5,
+        money: 100,
+      hot: false,
+    }]
+  }
 
 
-
-  },
+  ,
   // -----=---------------------------------------事件监听函数------------------------------------------------------------------
 
-  radioChange(e) {
-    this.setData({
-      goodsAddressTypeSelected: e.detail.value
-    })
-  },
   // 指定和就近地点标签转换
   switchTakeBtn(e) {
     this.setData({
       switchTakeTag: e.currentTarget.dataset.sindex,
-
     })
 
   },
+
+  // ---------------------------------------------跑腿费用窗口-------------------------------------------------------------
+  // 选择跑腿费用Tag
+  ChooseCheckbox(e) {
+    this.setData({
+        selectedCostTag: e.currentTarget.dataset.value
+    })
+  },
+  // 只是为了阻止冒泡，不需要实现
+  forStopBubbles(){},
   showModal: function(t) {
     this.setData({
       modalName: t.currentTarget.dataset.target
     });
   },
+  // 按下确定键
+  determineCost(){
 
+    this.setData({
+      userDeterminedCost: this.data.selectedCostTag==6?this.data.userFilledOtherCost:this.data.checkbox[this.data.selectedCostTag].money
+    })
+    this.hideModal();
+  },
   hideModal: function(t) {
     this.setData({
       modalName: null
     });
   },
+  // 按下其他金额
+  onOtherCost(){
+ this.setData({
+   OtherCostInputStaus:1,
+   selectedCostTag:6,
+   focusedOtherCost:true
+ })
+  },
+  // 失去焦点时，存储用户填写的其他金额
+  filledOtherCost(e){
+    console.log(e);
+    this.setData({
+      userFilledOtherCost:e.detail.value
+    })
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   abandonOrder(t) {
     this.setData({
