70e0e2a3778ed9ebf76a72d7032a409770afcdea
simpsun <oncwnuO4qbkQpI3ST2gMKlcdMQuw@git.weixin.qq.com>
2019-10-16 23:31:44 +0800

    101602

--- pages/graborder/itemOrderInfo/listOrderInfo.js
+++ pages/graborder/itemOrderInfo/listOrderInfo.js
@@ -5,62 +5,90 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    attrList: [{
+        name: '订单编号:',
+        value: '2019101600001'
+      },
+      {
+        name: '下单时间:',
+        value: '2019年10月16日 23:02'
+      },
+      {
+        name: '订单类型:',
+        value: '帮我买'
+      },
+      {
+        name: '赏金:',
+        value: '￥34.7'
+      },
+      {
+        name: '重量:',
+        value: '10~20Kg'
+      },
+      {
+        name: '性别限制:',
+        value: '不限男女'
+      },
+      {
+        name: '备注信息:',
+        value: '撒娇鲁大师卡打卡时口娇鲁大师卡打卡时口袋里卡塑料袋里来了的沙拉大说了输了第娇鲁大师卡打卡时口袋里卡塑料袋里来了的沙拉大说了输了第袋里卡塑料袋里来了的沙拉大说了输了第三轮'
+      } 
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
